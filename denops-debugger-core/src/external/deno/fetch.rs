5e0afb627369bc9d14762062917b71c7962a8e43
kkiyama117 <k.kiyama117@gmail.com>
2021-03-08 17:33:34 +0900

    Keep memo

--- denops-debugger-core/src/external/deno/fetch.rs
+++ denops-debugger-core/src/external/deno/fetch.rs
@@ -19,6 +19,7 @@ pub async fn fetch<T: DeserializeOwned>(url: &str) -> Result<T, anyhow::Error> {
                     let js_json = JsFuture::from(json);
                     match js_json.await {
                         Ok(result) => Ok(T::from(result.into_serde()?)),
+                        // TODO: use macro for convert
                         Err(e3) => Err(anyhow::Error::msg(format!("{:?}", e3.into_serde()?))),
                     }
                 }
