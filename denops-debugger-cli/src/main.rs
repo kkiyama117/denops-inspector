54e6065f0d0a8b8f81c9e86e52ef39855f56e9e1
kkiyama117 <k.kiyama117@gmail.com>
2021-03-08 16:24:30 +0900

    Keep

--- denops-debugger-cli/src/main.rs
+++ denops-debugger-cli/src/main.rs
@@ -3,12 +3,9 @@ use v8_inspector_api_types::prelude::{V8Version, WebSocketConnectionInfo};
 
 #[tokio::main]
 async fn main() {
-    dbg!(
-        fetch::<V8Version>("http://localhost:9229/json/version")
-            .await
-            .unwrap()
-            .v8_version,
-    );
+    dbg!(fetch::<V8Version>("http://localhost:9229/json/version")
+        .await
+        .unwrap());
     dbg!(
         fetch::<Vec<WebSocketConnectionInfo>>("http://localhost:9229/json")
             .await
