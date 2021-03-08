37da6c1ecf6e3ff640d348b2f2d497c8c22e46e8
kkiyama117 <k.kiyama117@gmail.com>
2021-03-08 16:02:58 +0900

    Refactor fetch

--- denops-debugger-core/tests/native.rs
+++ denops-debugger-core/tests/native.rs
@@ -2,12 +2,11 @@
 #[cfg(not(target_arch = "wasm32"))]
 mod tests {
     use denops_debugger_core::external::fetch::*;
-    use denops_debugger_core::*;
     use std::error::Error;
     use v8_inspector_api_types::prelude::*;
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
-    async fn it_works() {
+    async fn test_fetch() {
         // run_client("ws://127.0.0.1:9229/ws/8f27e285-769d-4a7d-aef0-55f506bd7fc6").await;
         assert_eq!(
             fetch::<V8Version>("http://localhost:9229/json/version")
@@ -16,6 +15,9 @@ mod tests {
                 .v8_version,
             "9.0.257.3"
         );
+        dbg!(fetch::<serde_json::Value>("http://localhost:9229/json")
+            .await
+            .unwrap());
         assert_eq!(2 + 2, 4);
     }
 }
